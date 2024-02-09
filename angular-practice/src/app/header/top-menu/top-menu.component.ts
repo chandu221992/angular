72531773e6fbef4f8b7c995ddee8e4d9b87904e4
Chandan Kumar -X (chandku5 - US Technology Resources Private Limited at Cisco) <chandku5@cisco.com>
2024-02-09 16:54:09 +0530

    my first angular project

---
+++ angular-practice/src/app/header/top-menu/top-menu.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'top-menu',
+  templateUrl: './top-menu.component.html',
+  styleUrls: ['./top-menu.component.css']
+})
+export class TopMenuComponent implements OnInit {
+  topMenuItems:string[]=['Help','Exchange & Returns','Order Tracker','Signup / Login']
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
